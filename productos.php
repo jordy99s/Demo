2cf86ff9a42563b047830a6cec5627303bb34fe7
Jordy Vargas <jordyvargas182@yahoo.com>
2021-11-22 17:47:47 -0600

    :sparkles: Jordy - Por fin hice la página de productos

--- productos.php
+++ productos.php
@@ -16,12 +16,125 @@
     <title>Productos</title>
 </head>
 <body>
+    <!-- CAMBIAR POR OTRO -->
     <!--HEADER-->
     <?php
         include 'lognavbar.php';
     ?>
     <!--CUERPO-->
-    <h3>HERE GOES PRODUCT RELATED INFORMATION</h3>
+    <div class="products">
+        <h1>Nuestros Productos</h1>
+        <div class="products-container">
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="images/prod/HOLCIM.png" alt="Cemento">
+                </div>
+                <div class="product-info">
+                    <p>Cemento Holcim Fuerte Gris</p>
+                    <div class="cantidad">
+                        <div class="c-text">
+                            Precio: C$ 409.36
+                        </div>
+                        <button class="btn-menos">-</button><input type="text" size="2" autofocus placeholder="1"><button class="btn-mas">+</button>
+                    </div>
+                    <div class="button-group">
+                        <button class="btn-add">Agregar al carrito</button>
+                        <button class="btn-ver">Ver más</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="images/prod/brocha_uro_cod_21380.png" alt="brocha">
+                </div>
+                <div class="product-info">
+                    <p>Brocha Pintura URO 5/8</p>
+                    <div class="cantidad">
+                        <div class="c-text">
+                            Precio: C$ 85.00
+                        </div>
+                        <button class="btn-menos">-</button><input type="text" size="2" autofocus placeholder="1"><button class="btn-mas">+</button>
+                    </div>
+                    <div class="button-group">
+                        <button class="btn-add">Agregar al carrito</button>
+                        <button class="btn-ver">Ver más</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="images/prod/Cemento cemex blanco 50kg.png" alt="cemex">
+                </div>
+                <div class="product-info">
+                    <p>Cemento Cemex Blanco</p>
+                    <div class="cantidad">
+                        <div class="c-text">
+                            Precio: C$ 999.99
+                        </div>
+                        <button class="btn-menos">-</button><input type="text" size="2" autofocus placeholder="1"><button class="btn-mas">+</button>
+                    </div>
+                    <div class="button-group">
+                        <button class="btn-add">Agregar al carrito</button>
+                        <button class="btn-ver">Ver más</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="images/prod/Anticorrosivo-Poliuretano-GLN-rojo-478x500.png" alt="corro">
+                </div>
+                <div class="product-info">
+                    <p>Anticorrosivo Poliuretano LANCO</p>
+                    <div class="cantidad">
+                        <div class="c-text">
+                            Precio: C$ 999.99
+                        </div>
+                        <button class="btn-menos">-</button><input type="text" size="2" autofocus placeholder="1"><button class="btn-mas">+</button>
+                    </div>
+                    <div class="button-group">
+                        <button class="btn-add">Agregar al carrito</button>
+                        <button class="btn-ver">Ver más</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="images/prod/pintura-lat-3100.png" alt="corro">
+                </div>
+                <div class="product-info">
+                    <p>Pintura SUR Látex 3100</p>
+                    <div class="cantidad">
+                        <div class="c-text">
+                            Precio: C$ 999.99
+                        </div>
+                        <button class="btn-menos">-</button><input type="text" size="2" autofocus placeholder="1"><button class="btn-mas">+</button>
+                    </div>
+                    <div class="button-group">
+                        <button class="btn-add">Agregar al carrito</button>
+                        <button class="btn-ver">Ver más</button>
+                    </div>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="images/prod/pinturas-sur-goltex-n-8.png" alt="corro">
+                </div>
+                <div class="product-info">
+                    <p>Pintura SUR Goltex</p>
+                    <div class="cantidad">
+                        <div class="c-text">
+                            Precio: C$ 999.99
+                        </div>
+                        <button class="btn-menos">-</button><input type="text" size="2" autofocus placeholder="1"><button class="btn-mas">+</button>
+                    </div>
+                    <div class="button-group">
+                        <button class="btn-add">Agregar al carrito</button>
+                        <button class="btn-ver">Ver más</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!--FOOTER-->
     <?php
         include 'footer.php';
