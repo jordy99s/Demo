e3f8174d277f06792f8536f9b8d77433f5955374
Jordy Vargas <jordyvargas182@yahoo.com>
2021-12-15 23:49:03 -0600

    :construction:

--- admin/inventario.php
+++ admin/inventario.php
@@ -190,7 +190,7 @@
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="example" width="100%" cellspacing="0">
+                                <table class="table table-bordered" id="tablaProducto" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th>ProductoId</th>
@@ -263,7 +263,7 @@
     <!-- Page level custom scripts -->
     <script type="text/javascript">
         $(document).ready(function() {
-            $('#example').DataTable( {
+            $('#tablaProducto').DataTable( {
                 "ajax" : {
                     "url" : "get_datap.php",
                     "dataSrc" : ""
