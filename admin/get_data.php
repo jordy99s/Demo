b4d9e2cc94b4dab78b946938efff639450c56eb6
Jordy Vargas <jordyvargas182@yahoo.com>
2021-12-15 22:24:01 -0600

    :bug: - Jordy - Error en tabla empleado. No mostraba rol Empleado

--- admin/get_data.php
+++ admin/get_data.php
@@ -1,7 +1,7 @@
 <?php
  
 $con = mysqli_connect("localhost", "root", "", "demo");
-$result = mysqli_query($con, "SELECT nombre, apellido, correo, rol FROM Usuarios WHERE rol = 'Administrador'");
+$result = mysqli_query($con, "SELECT nombre, apellido, correo, rol FROM Usuarios WHERE rol = 'Administrador' AND rol = 'Empleado'");
 $rows = array();
 while($row = mysqli_fetch_array($result)){
     $rows[] = $row;
